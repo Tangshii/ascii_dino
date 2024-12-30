992cf13e9e7e8219a1aad7d11cb2924307c29afd
Thang <99thangnguyen@gmail.com>
2024-12-30 00:05:34 -0500

    Removing Date.now() to try to load

--- src/asciiComponents/AsciiDriver.svelte
+++ src/asciiComponents/AsciiDriver.svelte
@@ -35,21 +35,21 @@
 		let frameCount = 0;
 		let lastFrameTime = 0;
 		function loop() {
-			let currentTime = Date.now();
-			let elapsedTime = currentTime - lastFrameTime;
+			// let currentTime = Date.now();
+			// let elapsedTime = currentTime - lastFrameTime;
 			
-			if(elapsedTime >= FPS_INTERVAL) {
+			// if(elapsedTime >= FPS_INTERVAL) {
 				frameCount++;
 				asciiGame.onTick(frameCount);
 				string = asciiGame.getString();
 				if(frameCount >= Number.MAX_VALUE) { // reset just in case
 					frameCount = 0;
 				}
-			}
+			// }
 			// Get ready for next frame by setting then=now, but...
         	// Also, adjust for fpsInterval not being multiple of 16.67
 			// from stack overflow
-			lastFrameTime = currentTime - (elapsedTime % FPS_INTERVAL);
+			// lastFrameTime = currentTime - (elapsedTime % FPS_INTERVAL);
 			window.requestAnimationFrame(loop);
 		}
 
